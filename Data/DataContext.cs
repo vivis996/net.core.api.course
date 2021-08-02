1eae6dfb87062642a5ff127a9fbb6a20ded647a3
Daniel Viveros <dviveros90@gmail.com>
2021-08-02 13:31:59 -0400

    30. Migration was created with its connection to DB

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using net.core.api.Models;
+
+namespace net.core.api.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Character> Characters { get; set; }
+  }
+}
