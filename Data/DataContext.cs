0685e122f3f4503f4ced9523e271de1b94c04229
Daniel Viveros <dviveros90@gmail.com>
2021-08-04 14:43:46 -0400

    59. Data Seeding - Add Skills to the Database

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -14,5 +14,14 @@ namespace net.core.api.Data
     public DbSet<User> Users { get; set; }
     public DbSet<Weapon> Weapons { get; set; }
     public DbSet<Skill> Skills { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Skill>().HasData(
+        new Skill { Id = 1, Name = "Force", Damage = 100, },
+        new Skill { Id = 2, Name = "Dark Rays", Damage = 110, },
+        new Skill { Id = 3, Name = "Push", Damage = 80, }
+      );
+    }
   }
 }
