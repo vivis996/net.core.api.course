a4d806db7e9c35d96f32be5ef92f24ade2f68a4b
Daniel Viveros <dviveros90@gmail.com>
2021-08-06 13:09:16 -0400

    125. This section is commented (only for reference to use), because use ChracterSkill model instead use directly (check video).

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -4,6 +4,7 @@ using net.core.api.Dtos.Fight;
 using net.core.api.Dtos.Skill;
 using net.core.api.Dtos.Weapon;
 using net.core.api.Models;
+using System.Linq;
 
 namespace net.core.api
 {
@@ -12,6 +13,7 @@ namespace net.core.api
     public AutoMapperProfile()
     {
       this.CreateMap<Character, GetCharacterDto>();
+            // .ForMember(dto => dto.Skills, c => c.MapFrom(c => c.CharacterSkills.Select(cs => cs.Skill)));
       this.CreateMap<AddCharacterDto, Character>();
       this.CreateMap<Weapon, GetWeaponDto>();
       this.CreateMap<Skill, GetSkillDto>();
